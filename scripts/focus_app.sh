36d7dad00bfb3a5b86fba4353ca38d1298cb70e7
Muke <misc.mikez@gmail.com>
2020-12-14 13:50:17 -0600

    update

---
+++ scripts/focus_app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -A appmap
+appmap["GoLand"]=goland
+appmap["Google Chrome"]=google-chrome-stable
+appmap["WebStorm"]=webstorm
+appmap["Terminal"]=gnome-terminal
+
+wmctrl -a $1
+if [[ $? -ne 0 ]]; then
+  ${appmap[$1]}
+fi
