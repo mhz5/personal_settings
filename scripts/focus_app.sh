3f52623eb483441a7fd8057235a6582ea9ba88fd
Mike <misc.mikez@gmail.com>
2020-12-21 11:32:11 -0600

    update

--- scripts/focus_app.sh
+++ scripts/focus_app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 declare -A appmap
-appmap["GoLand"]=goland
-appmap["Google Chrome"]=google-chrome-stable
-appmap["WebStorm"]=webstorm
-appmap["Terminal"]=gnome-terminal
+appmap["- GoLand"]=goland
+appmap["Google Chrome"]="google-chrome-stable --ignore-certificate-errors"
+appmap["- WebStorm"]=webstorm
+appmap["Terminal:"]=gnome-terminal
 
-wmctrl -a $1
+wmctrl -a "$1"
 if [[ $? -ne 0 ]]; then
   ${appmap[$1]}
 fi
