0349666d83d82668cf0ebeb984dc73dce0c62c1e
Mike <misc.mikez@gmail.com>
2020-12-23 10:01:59 -0600

    update

--- scripts/focus_app.sh
+++ scripts/focus_app.sh
@@ -5,6 +5,7 @@ appmap["- GoLand"]=goland
 appmap["Google Chrome"]="google-chrome-stable --ignore-certificate-errors"
 appmap["- WebStorm"]=webstorm
 appmap["Terminal:"]=gnome-terminal
+appmap["Sublime Text"]=sublime-text
 
 wmctrl -a "$1"
 if [[ $? -ne 0 ]]; then
